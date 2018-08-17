9adedc4a519c71b739ba2ce0f537b39aa5996105
Marko Požega <markopozega@floatingpoint.io>
2018-08-17 16:44:35 +0200

    Implement economic db selection for show action

--- lib/crudimentary/absinthe/generators/field.ex
+++ lib/crudimentary/absinthe/generators/field.ex
@@ -1,9 +1,9 @@
 defmodule CRUDimentary.Absinthe.Generator.Field do
   @moduledoc false
 
-  defmacro has_many(name, do: do_block) do
+  defmacro has_many(name, type \\ nil, do: do_block) do
     singularized_name = Inflex.singularize(name)
-
+    type = if type, do: type, else: singularized_name
     quote do
       field unquote(name), unquote(String.to_atom("#{singularized_name}_list_result")) do
         arg(:filter, list_of(unquote(String.to_atom("#{singularized_name}_filter"))))
